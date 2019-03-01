6f399e445cf1dcfb7782c0ef4bfe1b41c1ffe631
cc <chongwangcc@gmal.com>
2019-03-01 17:30:54 +0800

    [+功能] web与后台接通 能到点击打开各种tab了

--- MyCheckBook_backend/app.py
+++ MyCheckBook_backend/app.py
@@ -10,6 +10,8 @@ from flask import Flask, render_template, jsonify, request, redirect
 from flask_login.login_manager import LoginManager
 from flask_login import (current_user, login_required, login_user, logout_user, confirm_login, fresh_login_required)
 
+from tools import SqlTools
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123456'
@@ -21,28 +23,122 @@ def load_user(user_name):
     return SqlTools.fetch_user_info(user_name)
 
 
+@app.route("/index1", methods=["GET"])
+@login_required
+def index():
+    """
+    打开首页
+    :return:
+    """
+    return render_template('./index/index.html')
+
+
+@app.route("/overview", methods=["GET"])
+@login_required
+def overview():
+    """
+    查看概览
+    :return:
+    """
+    return render_template('./index/overview.html')
+
+
+@app.route("/checkbook_manage", methods=["GET"])
+@login_required
+def checkbook_manage():
+    """
+    记账本管理界面
+    :return:
+    """
+    return render_template('./index/checkbook_manage.html')
+
+
+@app.route("/detail_manage", methods=["GET"])
+@login_required
+def detail_manage():
+    """
+    明细挂了你界面
+    :return:
+    """
+    return render_template('./index/detail_manage.html')
+
+
+@app.route("/asset_manage", methods=["GET"])
+@login_required
+def asset_manage():
+    """
+    资产负债管理界面
+    :return:
+    """
+    return render_template('./index/asset_manage.html')
+
+
+@app.route("/report_manage", methods=["GET"])
+@login_required
+def report_manage():
+    """
+    财报管理界面
+    :return:
+    """
+    return render_template('./index/report_manage.html')
+
+
+@app.route("/budget_manage", methods=["GET"])
+@login_required
+def budget_manage():
+    """
+    预算管理界面
+    :return:
+    """
+    return render_template('./index/budget_manage.html')
+
+
+@app.route("/invest_manage", methods=["GET"])
+@login_required
+def invest_manage():
+    """
+    投资管理界面
+    :return:
+    """
+    return render_template('./index/invest_manage.html')
+
+
+@app.route("/trends_img", methods=["GET"])
+@login_required
+def trends_img():
+    """
+    走势图 界面
+    :return:
+    """
+    return render_template('./index/trends_img.html')
+
+
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     """
-    打开默认界面
+    登陆界面
     :return:
     """
     if request.method == 'POST':
-        user_name = request.form["username"]
-        password = request.form["password"]
-        # 判断用户密码是否正确
-        m_user = SqlTools.fetch_user_info(user_name)
-        if m_user is not None and m_user.password == password:
-            # 登陆成功
-            login_user(m_user, remember=True)
-            # 跳转
-            return redirect("/weeksum")
-        else:
-            # 登陆失败，弹出消息框
-            return render_template('login.html')
+        try:
+            user_name = request.form["username"]
+            password = request.form["password"]
+            # 判断用户密码是否正确
+            m_user = SqlTools.fetch_user_info(user_name)
+            if m_user is not None and m_user.password == password:
+                # 登陆成功
+                login_user(m_user, remember=True)
+                # 跳转
+                return redirect("index1")
+            else:
+                # 登陆失败，弹出消息框
+                return render_template('./index/login.html')
+        except:
+            pass
 
     # 判断有没有用户登录
-    return render_template('login.html')
+    return render_template('./index/login.html')
 
 
 @app.route("/logout", methods=["POST","GET"])
@@ -63,4 +159,8 @@ def page_not_found(e):
     :param e:
     :return:
     """
-    return render_template('404.html'), 40
+    return render_template('./index/404.html'), 40
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=7001)
--- MyCheckBook_backend/tools/SqlTools.py
+++ MyCheckBook_backend/tools/SqlTools.py
@@ -16,28 +16,11 @@ set_db_name(g_sqlite3_path)
 
 def fetch_user_info(user_name):
     """
-    获得用户信息类
+    TODO 获得用户信息类
     :param user_name:
     :return:
     """
     try:
-        userinfo = User_Info.get(user_name=user_name)
-        return userinfo
-    except:
-        pass
-    return None
-
-
-def insert_default_user():
-    """
-    插入默认的用户，只有当表内容为空时，才执行这个
-    :return:
-    """
-    if not User_Info.is_user_exist():
-        print("insert default user cc, mm")
-        global g_sqlite3_path
-        set_db_name(g_sqlite3_path)
-
         userinfo = User_Info()
         userinfo.user_name = "cc"
         userinfo.password = "123456"
@@ -45,174 +28,14 @@ def insert_default_user():
         userinfo.auth_token_file = r"./data/.credentials/cc_calendar.json"
         userinfo.calender_server = "google"
         userinfo.calender_name = "时间日志"
-        userinfo.save()
-
-        userinfo = User_Info()
-        userinfo.user_name="mm"
-        userinfo.password="123456"
-        userinfo.active=1
-        userinfo.auth_token_file = r"./data/.credentials/mm_calendar.json"
-        userinfo.calender_server = "google"
-        userinfo.calender_name = "时间日志"
-        userinfo.save()
-
-
-def get_time_details_df(user_id, start_date, end_date):
-    """
-    获得时间明细的记录
-    :param user_id:
-    :param start_date: 包含前者，包含后者
-    :param end_date:
-    :return: Dataframe
-    """
-    conn = sqlite3.connect(g_sqlite3_path)
-    sql = "select * from "+Time_Details.get_table_name()
-    sql += " where "
-    sql += " user_id == " +str(user_id) +" and "
-    sql += " date_str >= '" + start_date + "' "+" and "
-    sql += " date_str <= '" + end_date + "' "
-    time_details_df = pd.read_sql_query(sql, conn)
-    return time_details_df
-
-
-def update_time_detials_df(user_id, start_date_str, end_date_str, df_new):
-    """
-    保存dataframe 到数据库中，先删除，后插入
-    :param usid_id:
-    :param start_date_str:
-    :param end_date_str:
-    :param df_new:
-    :return:
-    """
-    # 创建表
-    is_update = False
-    update_date_list = []
-    Time_Details.try_create_table()
-    df_new["id"] = 0
-    # 从数据库中读旧内容
-    conn = sqlite3.connect(g_sqlite3_path)
-    sql = "select * from " + Time_Details.get_table_name()
-    sql += " where "
-    sql += " user_id == " + str(user_id) + " and "
-    sql += " date_str >= '" + start_date_str + "' " + " and "
-    sql += " date_str <= '" + end_date_str + "' "
-    df_old = pd.read_sql_query(sql, conn)
-    print("time_detials old_df :"+ str(len(df_old)))
-    print("time_detials df_new :" + str(len(df_new)))
-
-    # 比较两批dataframe 的不同之处
-    delete_df = df_old.append(df_new, sort=True)\
-                        .append(df_new, sort=True)\
-                        .drop_duplicates(subset=["only_key","md5"], keep=False)
-    update_df = df_new.append(df_old, sort=True)\
-                        .append(df_old, sort=True)\
-                        .drop_duplicates(subset=["only_key", "md5"], keep=False)\
-                        .drop_duplicates(subset=["only_key"], keep="first")
-    print("time_detials delete_df :" + str(len(delete_df)))
-    print("time_detials update_df :" + str(len(update_df)))
-    if len(delete_df)>0 or len(update_df)>0:
-        is_update = True
-    # 先删除，后更新  # 保存dataframe
-    [Time_Details.get(id=rows["id"]).delete() for index, rows in delete_df.iterrows()]
-
-    update_df = update_df.drop(columns=["id"])
-    update_df.to_sql(Time_Details.get_table_name(), conn, if_exists="append", chunksize=500, index=False)
-
-    # 构造返回结果：是否更新，变动的日期
-    update_date_list.extend(update_df["date_str"].tolist())
-    update_date_list.extend(delete_df["date_str"].tolist())
-    update_date_list = list(set(update_date_list))
-    update_date_list.sort()
-    return is_update, update_date_list
-
-
-def get_everyday_cache_df(user_id, start_date, end_date):
-    """
-    获得每天缓存的统计数据
-    :param user_id:
-    :param start_date:
-    :param end_date:
-    :return:Dataframe
-    """
-    conn = sqlite3.connect(g_sqlite3_path)
-    sql = "select * from "+Everyday_Cache.get_table_name()
-    sql += " where "
-    sql += " user_id == " +str(user_id) +" and "
-    sql += " date_str >= '" + start_date + "' "+" and "
-    sql += " date_str <= '" + end_date + "' "
-    df = pd.read_sql_query(sql, conn)
-    return df
-
-
-def update_everyday_cache_df(user_id, start_date_str, end_date_str, df_new):
-    """
-
-    :param user_id:
-    :param start_date_str:
-    :param end_date_str:
-    :param df_new:
-    :return:
-    """
-    is_update = False
-    update_date_list = []
-    Everyday_Cache.try_create_table()
-    # 从数据库中读旧内容
-    conn = sqlite3.connect(g_sqlite3_path)
-    sql = "select * from " + Everyday_Cache.get_table_name()
-    sql += " where "
-    sql += " user_id == " + str(user_id) + " and "
-    sql += " date_str >= '" + start_date_str + "' " + " and "
-    sql += " date_str <= '" + end_date_str + "' "
-    df_old = pd.read_sql_query(sql, conn)
-    print("everyday_cache df_old :" + str(len(df_old)))
-    print("everyday_cache df_new :" + str(len(df_new)))
-    # 比较两批dataframe 的不同之处
-    delete_df = df_old.append(df_new, sort=True) \
-        .append(df_new, sort=True) \
-        .drop_duplicates(subset=["only_key", "md5"], keep=False)
-    update_df = df_new.append(df_old, sort=True) \
-        .append(df_old, sort=True) \
-        .drop_duplicates(subset=["only_key", "md5"], keep=False) \
-        .drop_duplicates(subset=["only_key"], keep="first")
-    print("everyday_cache delete_df :" + str(len(delete_df)))
-    print("everyday_cache update_df :" + str(len(update_df)))
-    if len(delete_df) > 0 or len(update_df) > 0:
-        is_update = True
-    # 先删除，后更新  # 保存dataframe
-    [Everyday_Cache.get(id=rows["id"]).delete() for index, rows in delete_df.iterrows()]
-    update_df.to_sql(Everyday_Cache.get_table_name(), conn, if_exists="append", chunksize=500, index=False)
-
-    #构造返回结果：是否更新，变动的日期
-    update_date_list.extend(update_df["date_str"].tolist())
-    update_date_list.extend(delete_df["date_str"].tolist())
-    update_date_list = list(set(update_date_list))
-    update_date_list.sort()
-    return is_update, update_date_list
+        return userinfo
+    except:
+        pass
+    return None
 
 
 if __name__ == "__main__":
     pass
-    # create_table("drop")
-    # create_table()
-    # user_info = fetch_userInfo("cc")
-    # print(user_info)
-    # detail_list = []
-    # for i in range(10):
-    #     time_details=Every_week_Cache()
-    #     time_details.user_id = user_info.id
-    #     time_details.start_date_str = "2019-01-02"
-    #     time_details.end_date_str = "2019-01-02"
-    #     time_details.category ="运动"
-    #     time_details.during = 25
-    #     time_details.nums = 12
-    #     detail_list.append(time_details)
-    #
-    # save_everyweek_cache(user_info.id, "2019-01-02", detail_list)
-    #
-    # de = get_time_details(3, start_date="2019-01-01", end_date="2019-01-01")
-    # print(de)
-    insert_default_user()
-
     print(User_Info.is_user_exist())
 
 
