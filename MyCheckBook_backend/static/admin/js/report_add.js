1600f50611772e0588925716256b2618612ae599
cc <chongwangcc@gmail.com>
2019-03-22 19:30:04 +0800

    [+功能] 调试 steps 控件

--- MyCheckBook_backend/static/admin/js/report_add.js
+++ MyCheckBook_backend/static/admin/js/report_add.js
@@ -1,12 +1,32 @@
+layui.config({
+    debug: true
+    ,base: '../../static/admin/layui/extend/'
+}).extend({
+    steps:"steps/steps"
+});
 
 
-layui.use(['layer', 'jquery',"table", "laydate", "element", "form", 'steps'], function () {
-    var layer = layui.layer;
-    var table = layui.table;
-    var laydate = layui.laydate;
-    var $ = layui.jquery;
-    var element = layui.element;
-    var form = layui.form;
 
+
+layui.use(['jquery','steps'], function () {
+    // var layer = layui.layer;
+    // var table = layui.table;
+    // var laydate = layui.laydate;
+    // var $ = layui.jquery;
+    // var element = layui.element;
+    // var form = layui.form;
+ var $ = layui.$;
+
+    var $step= $("#step_demo").step();
+    //
+    $("#preBtn").click(function(event) {
+        $step.preStep();//上一步
+    });
+    $("#nextBtn").click(function(event) {
+        $step.nextStep();//下一步
+    });
+    $("#goBtn").click(function(event) {
+        $step.goStep(3);//到指定步
+    });
 
 })
--- MyCheckBook_backend/static/admin/layui/config.js
+++
@@ -1,9 +0,0 @@
-layui.config({
-    version: 222222
-    ,debug: true
-    ,base: '../../static/admin/layui/extend/'
-}).extend({
-    steps:"steps/steps"
-});
-
-
